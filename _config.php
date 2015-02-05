b75339e9f33c90438c731dcef6583f879cd068a4
Itayi Paul Patrick Chitovoro <patrick@chitosystems.com>
2015-02-05 09:30:29 +0200

    init commit

---
+++ _config.php
@@ -0,0 +1,3 @@
+<?php
+
+define('OWL_CAROUSEL_DIR', basename(dirname(__FILE__)));
---
+++ code/OwlCarousel.php
@@ -0,0 +1,13 @@
+<?php
+
+class OwlCarousel extends Object {
+
+    static function include_code() {
+        Requirements::javascript(OWL_CAROUSEL_DIR . '/js/owl.carousel.js');
+        Requirements::javascript(PROJECT . '/js/owl.carousel.init.js');
+        Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.carousel.css');
+        Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.theme.css');
+        Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.transitions.css');
+        Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.carousel.custom.css');        
+    }
+}
