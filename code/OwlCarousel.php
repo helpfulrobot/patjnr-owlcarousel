28089d15aaad47061472f5613dd4ebcb34a3f628
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:56:14 +1300

    Converted to PSR-2

--- code/OwlCarousel.php
+++ code/OwlCarousel.php
@@ -1,13 +1,15 @@
 <?php
 
-class OwlCarousel extends Object {
+class OwlCarousel extends Object
+{
 
-    static function include_code() {
+    public static function include_code()
+    {
         Requirements::javascript(OWL_CAROUSEL_DIR . '/js/owl.carousel.js');
         Requirements::javascript(OWL_CAROUSEL_DIR . '/js/owl.carousel.init.js');
         Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.carousel.css');
         Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.theme.css');
         Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.transitions.css');
-        Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.carousel.custom.css');        
+        Requirements::css(OWL_CAROUSEL_DIR . '/css/owl.carousel.custom.css');
     }
 }
